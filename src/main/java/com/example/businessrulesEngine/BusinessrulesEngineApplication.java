1b0685c184c3f06b76f71c6e68d3d0f31266cef4
aroma jason <you@example.com>
2022-01-09 20:55:58 +0000

    initial commit

---
+++ src/main/java/com/example/businessrulesEngine/BusinessrulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.businessrulesEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessrulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessrulesEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/businessrulesEngine/BusinessrulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessrulesEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessrulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
